67e3d8f0e7ddd1a970550d0899c0f0cce8f83008
cusnaruto <hoangvip3333@gmail.com>
2023-10-09 01:14:25 +0700

    update some css

--- GUIVersion/src/main/java/com/example/guiversion/HelloApplication.java
+++ GUIVersion/src/main/java/com/example/guiversion/HelloApplication.java
@@ -1,6 +1,8 @@
 package com.example.guiversion;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -22,38 +24,19 @@ public class HelloApplication extends Application {
         launch(args);
     }
     @Override
-    public void start(Stage stage) throws Exception {
-        Group root = new Group();
-        Scene scene = new Scene(root, 800, 600, Color.SLATEBLUE);
-        Image icon = new Image("C:\\Users\\cusnaruto\\Downloads\\Uni Stuffs\\OOP\\BigProject\\GUIVersion\\GUIVersion\\src\\FejwjyMVEAA5Ety.jpg");
-        Text text = new Text();
-        text.setText("fumo");
-        text.setX(400);
-        text.setY(300);
-        text.setFont(Font.font("Comic sans MS",50));
-        text.setFill(Color.YELLOW);
-        Line line = new Line();
-        line.setStartX(200);
-        line.setStartY(200);
-        line.setEndX(500);
-        line.setEndY(500);
-        line.setStrokeWidth(5);
-        line.setStroke(Color.RED);
-        Image image = new Image("C:\\Users\\cusnaruto\\Downloads\\Uni Stuffs\\OOP\\BigProject\\GUIVersion\\GUIVersion\\src\\gura.jpg");
-        ImageView imageView = new ImageView(image);
-        imageView.setX(0);
-        imageView.setY(0);
-        root.getChildren().add(imageView);
-        root.getChildren().add(text);
-        root.getChildren().add(line);
-        stage.getIcons().add(icon);
-        //stage.setWidth(720);
-        //stage.setHeight(1280);
-        //stage.setFullScreen(true);
-        stage.setFullScreenExitHint("suck my c-");
-        stage.setResizable(false);
-        stage.setTitle("chit fumo");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+            Scene scene = new Scene(root);
+            String css = this.getClass().getResource("/Application.css").toExternalForm();
+            scene.getStylesheets().add(css);
+            Image icon = new Image("C:\\Users\\cusnaruto\\Downloads\\Uni Stuffs\\OOP\\BigProject\\GUIVersion\\GUIVersion\\src\\icon.jpg");
+            stage.getIcons().add(icon);
+            stage.setTitle("chit fumo");
+            stage.setScene(scene);
+            stage.show();
+        } catch(Exception e) {
+        e.printStackTrace();
+        }
     }
 }
